a8085576bd49c3dda3b85b8f6458f984cdcc3a6a
reim-weib-hr <hrrmanager985@gmail.com>
2024-03-08 06:17:39 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { Cyber } from './cyber';
+import { Confuse } from './confuse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Confuse />
   </React.StrictMode>
 );
 
